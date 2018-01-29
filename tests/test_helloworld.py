98241c1cb1a6db1b234e72da47880452375fa183
Kit Randel <kit@nocturne.net.nz>
2018-01-29 20:04:45 +1300

    Add button click test.

--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -1,16 +1,24 @@
 import unittest
 
-import helloworld
+from selenium import webdriver
 
 
-class HelloworldTestCase(unittest.TestCase):
-
+class HelloworldBrowserTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = helloworld.app.test_client()
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.driver = webdriver.Chrome(options=options)
+        self.baseURL = "http://localhost:5000/"
 
-    def test_index(self):
-        rv = self.app.get('/')
-        self.assertIn('Welcome to flask-hello-world', rv.data.decode())
+    def tearDown(self):
+        self.driver.quit
+
+    def test_button_text(self):
+        self.driver.get(self.baseURL)
+        button = self.driver.find_element_by_id('clickMe')
+        button.click()
+        self.assertIn('You clicked the button.',
+                      self.driver.find_element_by_id('result').text)
 
 
 if __name__ == '__main__':
